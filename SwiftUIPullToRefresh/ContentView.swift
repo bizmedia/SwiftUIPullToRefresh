3c532614f7ce58adfa97956310db3eb1cd228963
Анатолий Туков <at@bizmedia.ru>
2019-11-04 18:25:52 +0300

    Initial Commit

---
+++ SwiftUIPullToRefresh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPullToRefresh
+//
+//  Created by Туков Анатолий on 04.11.2019.
+//  Copyright © 2019 Туков Анатолий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
